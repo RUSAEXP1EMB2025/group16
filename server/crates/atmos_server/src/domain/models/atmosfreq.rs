8db1c1e6fbc1dd8984fde016d2ed475ef743ca51
crcrworks <42961885+crcrworks@users.noreply.github.com>
2025-06-07 14:35:42 +0900

    feat: atmosfreqのテストの骨組みを実装

--- server/crates/atmos_server/src/domain/models/atmosfreq.rs
+++ server/crates/atmos_server/src/domain/models/atmosfreq.rs
@@ -1,8 +1,8 @@
 use derive_more::From;
 
-// 雰囲気指数 (0 ~ 100)
-#[derive(PartialEq, Eq, PartialOrd, Ord, From)]
-pub struct AtmosFreq(i32);
+// 雰囲気指数 (-100.0 ~ 100.0)
+#[derive(PartialEq, Debug, From)]
+pub struct AtmosFreq(f64);
 
 impl AtmosFreq {
     pub fn new(texts: &Vec<String>) -> Self {
@@ -13,8 +13,8 @@ impl AtmosFreq {
 impl From<&Vec<String>> for AtmosFreq {
     fn from(texts: &Vec<String>) -> Self {
         // TODO: テキスト群から雰囲気指数を計算する。amountは0 ~ 100以内にする
-        let amount: i32 = todo!();
-        let amount = amount.clamp(0, 100);
+        let amount: f64 = todo!();
+        let amount = amount.clamp(-100.0, 100.0);
 
         AtmosFreq(amount)
     }
@@ -24,9 +24,39 @@ impl From<&Vec<String>> for AtmosFreq {
 mod test {
     use super::AtmosFreq;
 
+    fn mock_text(texts: impl Into<Vec<&'static str>>) -> Vec<String> {
+        let texts: Vec<&str> = texts.into();
+        let texts = texts.iter().map(|&s| String::from(s)).collect();
+        texts
+    }
+
     #[test]
-    fn test_valid_atmosfreq() {
-        let atmosfreq = AtmosFreq::from(0);
-        assert!((0..100).contains(&atmosfreq.0))
+    fn test_calc_atmosfreq_1() {
+        // TODO: テストケースの実装
+        let texts = mock_text(["a", "b", "c"]);
+        let expect_result = 0.0;
+
+        let atmosfreq = AtmosFreq::new(&texts);
+        assert_eq!(atmosfreq, AtmosFreq::from(expect_result))
+    }
+
+    #[test]
+    fn test_calc_atmosfreq_2() {
+        // TODO: テストケースの実装
+        let texts = mock_text(["a", "b", "c"]);
+        let expect_result = 0.0;
+
+        let atmosfreq = AtmosFreq::new(&texts);
+        assert_eq!(atmosfreq, AtmosFreq::from(expect_result))
+    }
+
+    #[test]
+    fn test_calc_atmosfreq_3() {
+        // TODO: テストケースの実装
+        let texts = mock_text(["a", "b", "c"]);
+        let expect_result = 0.0;
+
+        let atmosfreq = AtmosFreq::new(&texts);
+        assert_eq!(atmosfreq, AtmosFreq::from(expect_result))
     }
 }
